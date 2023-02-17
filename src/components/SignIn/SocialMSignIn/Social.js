14dc2ee87faa059e95ca4b10a65227730dcf4d44
lilibella <lilibella28@hotmail.com>
2023-02-17 14:19:18 -0500

    social media  login option set up

--- src/components/SignIn/SocialMSignIn/Social.js
+++ src/components/SignIn/SocialMSignIn/Social.js
@@ -3,23 +3,29 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { MDBIcon } from 'mdb-react-ui-kit';
 import Button from '../../Common/Button/Button';
-import '../../../scss/SocialStyle/SocialStyle.scss'
+import '../../../scss/SocialStyle/SocialStyle.scss';
 function Social() {
 	return (
 		<div>
 			<Container>
 				<Row>
-					<Button  className ="google-button my-button" type="button">
-						{' '}
-						<MDBIcon fab icon="google" /> Continue with Google
+					<Button className="google-button my-button" type="button">
+						<span>
+							{' '}
+							<MDBIcon fab icon="google" /> Continue with Google
+						</span>
 					</Button>
-					<Button  className="facebook-button my-button" type="button">
-						{' '}
-						<MDBIcon fab icon="facebook" /> Continue with Facebook
+
+					<Button className="facebook-button my-button" type="button">
+						<span>
+							{' '}
+							<MDBIcon fab icon="facebook" /> Continue with Facebook
+						</span>
 					</Button>
 					<Button className="apple-button my-button" type="button">
-						{' '}
-						<MDBIcon fab icon="apple" /> Continue with Apple
+						<span>
+							<MDBIcon fab icon="apple" /> Continue with Apple
+						</span>
 					</Button>
 				</Row>
 			</Container>
