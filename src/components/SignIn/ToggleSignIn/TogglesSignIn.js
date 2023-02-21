b6e67164d836eb118b1e0464ed190b98b503217d
lilibella <lilibella28@hotmail.com>
2023-02-21 17:48:03 -0500

    update swicth componets to render login and sign  up  form

--- src/components/SignIn/ToggleSignIn/TogglesSignIn.js
+++ src/components/SignIn/ToggleSignIn/TogglesSignIn.js
@@ -2,21 +2,32 @@ import React, { useState } from 'react';
 import SignIn from '../SigInMain/SigInMain';
 import EmailForm from '../../SignUp/SignUp';
 import Container from 'react-bootstrap/Container';
-import Button from '../../Common/Button/Button'
-function ToggleButton() {
-	const [isOn, setIsOn] = useState(false);
+import '../../../scss/ToggleStyle/ToggleStyle.scss'
+import Switch from 'react-switch'
 
-	const handleClick = () => {
-		setIsOn(!isOn);
+function ToggleButton() {
+	const [showLogin, setShowLogin] = useState(true);
+	const handleSwitch = () => {
+		setShowLogin(!showLogin);
 	};
 
 	return (
-		<Container>
-		<div>
-          <h6>Sign in or Sign Up</h6>
-			<Button onClick={handleClick}>{isOn ? 'Sign Up' : 'Sign In'}</Button>
-			{isOn ? <EmailForm /> : <SignIn/>}
-		</div>
+		<Container  className="MainContainer">
+			<h6>Sign in or Sign Up</h6>
+			<div className="auth-toggle">
+				<span>Sign in</span>
+				<Switch
+					onChange={handleSwitch}
+					checked={!showLogin}
+					uncheckedIcon={false}
+					checkedIcon={false}
+					offColor="#3BCEAC"
+					onColor="#OEAD69"
+					
+				/>
+				<span>Sign Up</span>
+			</div>
+			{showLogin ? <SignIn /> : <EmailForm />}
 		</Container>
 	);
 }
