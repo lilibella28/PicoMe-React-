ce12b37e30e34809ff9084fffb0d101c13253e9b
lilibella <lilibella28@hotmail.com>
2023-02-23 23:57:55 -0500

    fixed export

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-function Header(){
-    return(
-       <div>
-           <h1>Offer food</h1>
-       </div>
-    );
-};
+function Header() {
+	return (
+		<div>
+			<h1>Offer food</h1>
+		</div>
+	);
+}
 
-export default OfferFood;
+export default Header;
