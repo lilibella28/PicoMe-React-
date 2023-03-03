e9188e5d3a65deb22e222fd94a235b8810b06850
lilibella <lilibella28@hotmail.com>
2023-03-03 15:05:04 -0500

    adding states to handles user's data

---
+++ src/components/HomePage/UsersCards/UsersCards.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import React, { useState } from 'react';
+
+function UsersCards(){
+    
+    const [username, setUsername] = useState()
+    const [userPicture, setUserPicture]= useState()
+    const [address, setAdress] = useState()
+    const [date, setDate] = useState()
+    const [heading, setHeading] = useState()
+    const [text, setText] = useState()
+
+
+}
+
+export default  UsersCards
