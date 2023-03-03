96f07ac6a94a3a1f770c030dde02bf39c2272b19
lilibella <lilibella28@hotmail.com>
2023-03-03 16:02:52 -0500

    added new format for userData

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import UsersCards from './UsersCards/UsersCards.js'
 import Map from '../Map/Map'
 function HomePage() {
 	return (
@@ -9,9 +10,8 @@ function HomePage() {
 			
 			<Container fluid>
       <Row>
-		 
-        <Col>
-		<h1>Hello</h1>
+		 <Col>
+        <UsersCards/>
 		 </Col>
 		 
 		
--- src/components/HomePage/UsersCards/UsersCards.js
+++ src/components/HomePage/UsersCards/UsersCards.js
@@ -1,16 +1,39 @@
-import React from 'react'
 import React, { useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function UsersCards(){
-    
-    const [username, setUsername] = useState()
-    const [userPicture, setUserPicture]= useState()
-    const [address, setAdress] = useState()
-    const [date, setDate] = useState()
-    const [heading, setHeading] = useState()
-    const [text, setText] = useState()
+function UsersCard() {
+	const [userData, setUserData] = useState({
+		username: 'lili',
+		userPicture:
+			'https://images.pexels.com/photos/7245308/pexels-photo-7245308.jpeg?auto=compress&cs=tinysrgb&w=168',
+		address: 'San Francisco, CA',
+		date: '12:00pm, 11/04/1995',
+		heading: 'Hey Picos! I need your Help',
+		text:
+			"Pico community, I'm in the middle of making lasagna but I'm missing one important ingredient - ground beef. If anyone has any spare, I would be immensely grateful for your help.",
+	});
 
-
+	return (
+		<div>
+			<Row>
+				<Col>
+					<img src={userData.userPicture} alt="User profile" />
+				</Col>
+				<Col>
+					<h5>{userData.heading}</h5>
+					<h5>{userData.username}</h5>
+					<p>{userData.text}</p>
+				</Col>
+				<Col>
+					<h5>Location:</h5>
+					<h6>{userData.address}</h6>
+					<h5>Date:</h5>
+					<h6>{userData.date}</h6>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
-export default  UsersCards
+export default UsersCard;
