07585f490ebae0ac0af89bf094e7ebef44f5947b
lilibella <lilibella28@hotmail.com>
2023-04-18 12:26:02 -0400

    update social media icon to used Tailwind and delete ssass styling

--- src/components/SignIn/SocialMSignIn/Social.js
+++ src/components/SignIn/SocialMSignIn/Social.js
@@ -3,26 +3,25 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { MDBIcon } from 'mdb-react-ui-kit';
 import Button from '../../Common/Button/Button';
-import '../../../scss/SocialStyle/SocialStyle.scss';
 function Social() {
 	return (
-		<div className="MainSocial">
+		<div className="relative left-2.5 right-6">
 			<Container>
 				<Row>
-					<Button className="google-button my-button" type="button">
+					<Button className="bg-google-bg flex justify-center max-w-full min-w-min rounded-full border-none text-white items-center text-center m-0 p-0 mt-2 font-semibold" type="button">
 						<span>
 							{' '}
 							<MDBIcon fab icon="google" /> Continue with Google
 						</span>
 					</Button>
 
-					<Button className="facebook-button my-button" type="button">
+					<Button className="bg-facebook-bg flex justify-center max-w-full min-w-min rounded-full border-none text-white items-center text-center m-0 p-0 mt-2 font-semibold" type="button">
 						<span>
 							{' '}
 							<MDBIcon fab icon="facebook" /> Continue with Facebook
 						</span>
 					</Button>
-					<Button className="apple-button my-button" type="button">
+					<Button className="bg-black mb-5 flex justify-center max-w-full min-w-min rounded-full border-none text-white items-center text-center m-0 p-0 mt-2 font-semibold" type="button">
 						<span>
 							<MDBIcon fab icon="apple" /> Continue with Apple
 						</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+       'google-bg': 'rgb(66, 133, 246)',
+       'facebook-bg': 'rgb(59, 89, 152)'
+      }
+    },
   },
   plugins: [],
 }
