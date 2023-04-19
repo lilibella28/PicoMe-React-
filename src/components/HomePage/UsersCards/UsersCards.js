96c66641c45dfb075d5f7672d48fb127829e56a4
lilibella <lilibella28@hotmail.com>
2023-04-19 17:56:46 -0400

    Usercards event handlers added

--- src/components/HomePage/UsersCards/UsersCards.js
+++ src/components/HomePage/UsersCards/UsersCards.js
@@ -14,31 +14,51 @@ function UsersCard() {
 		text:
 			"Pico community, I'm in the middle of making lasagna but I'm missing one important ingredient - ground beef. If anyone has any spare, I would be immensely grateful for your help.",
 	});
+  const acceptHandler = () =>{
+    console.log("hello")
+  }
 
 	return (
 		<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 			
-    <div class="relative  z-10  transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl">
-    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-      <div class="sm:flex sm:items-start">
-        <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-200 sm:mx-0 sm:h-10 sm:w-10">
+    <div className="relative  z-10  transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl">
+    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+      <div className="sm:flex sm:items-start">
+        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-200 sm:mx-0 sm:h-10 sm:w-10">
           
-          <img class="h-8 w-8 text-red-600  rounded-full" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"src={userData.userPicture} alt="User profile" />
+          <img className="h-8 w-8 text-red-600  rounded-full" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"src={userData.userPicture} alt="User profile" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
           
         </div>
-        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-          <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">{userData.heading}</h3>
-          <div class="mt-2">
-            <p class="text-sm text-gray-500">{userData.text}</p>
+        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+          <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">{userData.heading}</h3>
+          <div className="mt-2">
+            <p className="text-sm text-gray-500">{userData.text}</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-      <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-orange-500 sm:mt-0 sm:w-auto">Accept Request</button>
+    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 justify-between">
+      <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-orange-500 sm:mt-0 sm:w-auto">Accept Request</button>
+      
+      <div className="flex flex-row-reverse justify-evenly items-center  text-xs text-semibold">
+      <h6 className="text-base text-xs">{userData.address}</h6>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+ 
+</svg>
+ </div>
+ <div className="flex flex-row-reverse justify-evenly items-center text-base text-xs">
+ <h6 className="text-base text-xs">{userData.date}</h6>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+  <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clipRule="evenodd" />
+</svg>
+ </div>
+</div>
+    
 
-    </div>
+  
   </div>
  </div>
 	);
